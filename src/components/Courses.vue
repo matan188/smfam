94313b49cd64c7f1280c8b6d9b15f0a67d98427a
Matan <matan.modai@gmail.com>
2019-04-12 10:50:29 +0300

    Export filter elements to FilterCourses component

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,50 +1,17 @@
 <template>
   <div>
     <v-container>
-      <v-layout>
-        <v-flex lg2 mr-2>
-          <v-combobox
-            label="Category"
-            v-model="category"
-            :items="categories"
-            multiple
-            placeholder="All"
-          >
-          </v-combobox>
-        </v-flex>
-        <v-flex lg2>
-          <v-combobox
-            label="Author"
-            v-model="author"
-            :items="authors"
-            multiple
-            placeholder="All"
-          >
-          </v-combobox>
-        </v-flex>
-        <v-spacer></v-spacer>
-        <v-flex lg4>
-          <v-text-field
-            solo
-            placeholder="Find a course"
-            prepend-inner-icon="search"
-          />
-        </v-flex>
-      </v-layout>
+      <FilterCourses />
     </v-container>
   </div>
 </template>
 <script>
+import FilterCourses from '@/components/FilterCourses';
 export default {
   name: 'Courses',
 
-  data() {
-    return {
-      category: [],
-      categories: ['Calisthenics', 'Yoga'],
-      author: [],
-      authors: ['Gabo Saturno', 'The SM Team']
-    };
+  components: {
+    FilterCourses
   }
 };
 </script>
---
+++ src/components/FilterCourses.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-layout>
+    <v-flex lg2 mr-2>
+      <v-combobox
+        label="Category"
+        v-model="category"
+        :items="categories"
+        multiple
+        placeholder="All"
+      >
+      </v-combobox>
+    </v-flex>
+    <v-flex lg2>
+      <v-combobox
+        label="Author"
+        v-model="author"
+        :items="authors"
+        multiple
+        placeholder="All"
+      >
+      </v-combobox>
+    </v-flex>
+    <v-spacer></v-spacer>
+    <v-flex lg4>
+      <v-text-field
+        solo
+        placeholder="Find a course"
+        prepend-inner-icon="search"
+      />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'FilterCourses',
+
+  data() {
+    return {
+      category: [],
+      categories: ['Calisthenics', 'Yoga'],
+      author: [],
+      authors: ['Gabo Saturno', 'The SM Team']
+    };
+  }
+};
+</script>
