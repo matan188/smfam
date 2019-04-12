2ead1b7a19629169374d603925af319d3a19bb95
Matan <matan.modai@gmail.com>
2019-04-12 23:58:23 +0300

    Course card styles parity

--- src/components/CoursesCards.vue
+++ src/components/CoursesCards.vue
@@ -2,13 +2,33 @@
   <v-container class="grid-list-xl px-0" fluid>
     <v-layout row wrap>
       <v-flex lg4 v-for="n in 9" :key="n">
-        <v-card>
+        <v-card hover>
           <v-img
-            :src="require('@/assets/calisthenicsinitiation.jpg')"
+            :src="require('@/assets/calisthenics-initiation.jpg')"
             height="200px"
           ></v-img>
           <v-card-title primary-title>
             <div class="headline">Calisthenics Initiation Phase</div>
+            <v-progress-linear
+              v-model="progress"
+              color="deep-purple"
+            ></v-progress-linear>
+            <v-layout row align-center>
+              <v-flex md1 class="mr-2">
+                <v-avatar size="30">
+                  <img
+                    :src="require('@/assets/gabo-profile.jpg')"
+                    alt="Gabo Saturno"
+                  />
+                </v-avatar>
+              </v-flex>
+              <v-flex md5>
+                Gabo Saturno
+              </v-flex>
+              <v-flex md2 offset-md5>
+                <div>30%</div>
+              </v-flex>
+            </v-layout>
           </v-card-title>
         </v-card>
       </v-flex>
@@ -17,6 +37,12 @@
 </template>
 <script>
 export default {
-  name: 'CoursesCards'
+  name: 'CoursesCards',
+
+  data() {
+    return {
+      progress: 30
+    };
+  }
 };
 </script>
