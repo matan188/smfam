f8a611299b2f2bd7bc1d9f53026f1d4e5fb58d41
Matan <matan.modai@gmail.com>
2019-04-13 17:46:50 +0300

    Add Firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+// Initialize Firebase
+var config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
+firebase.initializeApp(config);
