a75485a8e3b32af26983b7d3e59358ce1bef13e8
Matan <matan.modai@gmail.com>
2019-04-13 19:59:55 +0300

    Put Store in designated folder

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import './plugins/vuetify';
+import '@/firebase';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import '@/firebase';
 
 Vue.config.productionTip = false;
 
