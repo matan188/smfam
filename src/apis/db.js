9fc2556392388802fa9025d8e53576d5633ce1ef
Matan <matan.modai@gmail.com>
2019-04-23 00:00:58 +0300

    db method to use async await

--- src/apis/db.js
+++ src/apis/db.js
@@ -4,22 +4,17 @@ import 'firebase/firestore';
 const firestore = firebase.firestore();
 
 class DB {
-  getAllCourses() {
-    return firestore
-      .collection('courses')
-      .get()
-      .then(query => {
-        console.log('docs', query.docs);
-
-        const courses = query.docs.map(doc => {
-          return {
-            id: doc.id,
-            ...doc.data()
-          };
-        });
-        console.log('courses', courses);
-        return courses;
-      });
+  async getAllCourses() {
+    const query = await firestore.collection('courses').get();
+    console.log('docs', query.docs);
+    const courses = query.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      };
+    });
+    console.log('courses', courses);
+    return courses;
   }
 }
 
